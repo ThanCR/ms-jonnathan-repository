6b51b4b4832386baa50082eca34b273b53e3e244
Jonnathan Rios <jonzxz12@gmail.com>
2023-01-25 15:23:01 +0000

    ExceptionMaker app development started:
    --added DivisionByZero exception
    --added StackOverflow exception
    --pending to finish Memory leak

---
+++ exceptions-maker-app/ExceptionMaker/ExceptionMaker/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using ExceptionMaker.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ExceptionMaker.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MathExceptionGeneration()
+        {
+
+            const int valueA = 10;
+            const int valueB = 2;
+            ViewBag.result = executeDivision(valueA, valueB - 2);
+            return View();
+        }
+
+        public IActionResult StackOverflowExceptionGeneration()
+        {
+            executeOverflow(1);
+            return View();
+        }
+
+        public IActionResult MemoryLeakGeneration() 
+        {
+            generateMemoryLeak();
+            return View();    
+        }
+
+        private int executeDivision(int valueA, int valueB)
+        {
+            return valueA / valueB;
+        }
+
+        private void executeOverflow(int num)
+        {
+            int value = num++;
+            executeOverflow(value);
+        }
+
+        public static void generateMemoryLeak()
+        {
+            string[] values = { };
+           
+            
+            for (int x = 0;  values.Length < 10000000; x++)
+            {
+                values[x] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- exceptions-maker-app/Readme.md
+++ exceptions-maker-app/Readme.md
@@ -1,17 +1,17 @@
-#Exception Generating app
+# Exception Generating app
 
 
-##Session 1
+## Session 1
     1. Programming fundamentals and main concepts
     2. Variables, classes, methods, usings and packages, constructors and control structures
     3. Brief lab, Windows forms or console application
 
-##Session 2
+## Session 2
     1. Exceptions handling, try and catch, types of exceptions, runtime, common causes
     2. Understanding error information
     3. Brief lab, error generating application 
 
-##Session 3
+## Session 3
     1. Hosting error generating application on IIS
     2. IIS behavior when facing errors
     3. Dump taking with DebugDiag and ProcDump
